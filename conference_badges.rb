edd9ffbff5b0b55c147d1436c39fdd30652dc9f5
Dazha Brown <dajahbrown@hotmail.com>
2020-05-12 22:43:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,10 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
- def batch_badge_creator(attendees)
-  badge_attendees = [attendees]
-  return badge_messages = []
-  badge_attendees.map {|name| badge_messages << badge_maker(name)}
- return badge_messages
+ def batch_badge_creator(array)
+  badge_attendees = []
+  array.each do |name|
+    batch_array.push ("Hello, my name is #{name}.")
+  end
+ return batch_array
  end
